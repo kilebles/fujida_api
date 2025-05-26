6488f03689b6d43639c164448a9c71f5fc32b434
Elisey <eliseyblajen@gmail.com>
2025-05-27 03:58:23 +0500

    Add postgres container

--- app/fujida_api/config.py
+++ app/fujida_api/config.py
@@ -5,6 +5,12 @@ class Settings(BaseSettings):
     APP_HOST: str
     APP_PORT: int
     
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    
     model_config = SettingsConfigDict(env_file=".env")
     
 
