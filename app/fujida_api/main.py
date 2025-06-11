87ad5af807652d3bf6d78574cdcc473168896264
Elisey <eliseyblajen@gmail.com>
2025-06-12 02:20:43 +0500

    Add admin panel

--- app/fujida_api/main.py
+++ app/fujida_api/main.py
@@ -2,9 +2,17 @@ import uvicorn
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import create_engine
+from sqladmin import Admin, ModelView
 
 from app.fujida_api.config import config
 from app.fujida_api.routes import router as api_router
+
+from app.fujida_api.db.models import FAQEntry
+
+SYNC_DATABASE_URL = config.DATABASE_URL.replace('postgresql+asyncpg', 'postgresql')
+
+sync_engine = create_engine(SYNC_DATABASE_URL, echo=False)  # под Админку синхронный
 
 app = FastAPI(title='fujida_api')
 
@@ -18,6 +26,13 @@ app.add_middleware(
 )
 
 app.include_router(api_router)
+admin = Admin(app, sync_engine)
+
+class FAQEntryAdmin(ModelView, model=FAQEntry):
+    column_list = [FAQEntry.id, FAQEntry.question, FAQEntry.answer]
+    form_columns = ['question', 'answer']
+
+admin.add_view(FAQEntryAdmin)
 
 if __name__ == '__main__':
     uvicorn.run(
